01252c1e33aa29cd977591eac61d7dbf6f9bc831
Ainé Figueroa <aine.xena.100@gmail.com>
2025-10-17 09:45:50 +0200

    empezamos con lo de router

---
+++ src/components/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="row">
+        <div class="col-12 text-center">
+            <h1 class="display-1"> 401 </h1>
+            <img src="@/assets/homer-error.png" alt="404 Error" class="mb-4" />
+            <h2 class="mb-4"> Pçagina no Encontrada </h2>
+            <p class="mb-4"> Perdona, nos hemos equivocado </p>
+            <router-link to="/" class="btn btn-primary border-0 shadow-none"> Ir a Inicio </router-link>
+        </div>
+    </div>
+</template>
---
+++ src/components/PaginaInicio.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2> Página Inicio Empresa Teis </h2>
+    </div>
+</template>
+<script setup></script>
+<style scoped></style>
