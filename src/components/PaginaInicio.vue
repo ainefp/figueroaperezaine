b5ca563959e0504214d813cd8fdad363d425ed73
Ainé Figueroa <aine.xena.100@gmail.com>
2025-10-21 09:51:59 +0200

    empiezo con la página de inicio

--- src/components/PaginaInicio.vue
+++ src/components/PaginaInicio.vue
@@ -1,7 +1,27 @@
 <template>
-    <div>
-        <h2> Página Inicio Empresa Teis </h2>
+    <div class="div">
+        <img src="../assets/imagen1.avif" alt="gr" class="inicio-img" />
+        <h2 class="inicio-title"> Moto's Concesionario </h2>
     </div>
 </template>
-<script setup></script>
-<style scoped></style>
+<script setup>
+    import imagen1 from '@/assets/imagen1.avif';
+    const imagenes = [imagen1]
+</script>
+<style scoped>
+    .div {
+        display:flex;
+        justify-content: center;
+    }
+    .inicio-title {
+        position: absolute;
+        margin-top: 40px;
+        font-weight: 600;
+        text-transform: uppercase;
+        font-size: 28px;
+        letter-spacing: 1px;
+    }
+    .inicio-img {
+        width: 100%;
+    }
+</style>
