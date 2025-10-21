d4ca7d11ae7af65e2c086716297c5302d82d458c
Ainé Figueroa <aine.xena.100@gmail.com>
2025-10-21 09:51:34 +0200

    quitamos importación de bootstrap  para que el menú cierre

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from './App.vue'
 import router from './router'
 
 // Bootstrap CSS + JS (solo bundle)
-import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
