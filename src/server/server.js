63c3e9bac6dc8e3f11d0b9d799ab939283c27345
Ainé Figueroa <aine.xena.100@gmail.com>
2025-10-30 08:54:41 +0100

    instalamos nuevas funciones

---
+++ src/server/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+
