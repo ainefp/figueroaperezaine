e20230c53f2fc7a1326085e28239a8fd54d4c9bf
Ainé Figueroa <aine.xena.100@gmail.com>
2025-10-30 09:47:46 +0100

    movido .env y cambiado nombre de la variable

--- src/server/server.js
+++ src/server/server.js
@@ -7,12 +7,12 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
+const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/bbdd';
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
