6242643ab0758057c4b7157ce33f7903252fb8a1
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-04 09:24:29 +0100

    añadido target="_blank" a aviso legal

--- src/components/FooTer.vue
+++ src/components/FooTer.vue
@@ -3,8 +3,8 @@
     <div class="container">
       <p class="mb-1">&copy; {{ new Date().getFullYear() }} MiSitio. Todos los derechos reservados.</p>
       <ul class="list-inline mb-0">
-        <li class="list-inline-item"><router-link to="/avisolegal" class="text-light text-decoration-none">Aviso legal</router-link></li>
-        <li class="list-inline-item"><router-link to="/politicadeprivacidad" class="text-light text-decoration-none">Política de privacidad</router-link></li>
+        <li class="list-inline-item"><router-link to="/avisolegal" target="_blank" class="text-light text-decoration-none">Aviso legal</router-link></li>
+        <li class="list-inline-item"><router-link to="/politicadeprivacidad" target="_blank" class="text-light text-decoration-none">Política de privacidad</router-link></li>
         <li class="list-inline-item"><a href="#" class="text-light text-decoration-none">Contacto</a></li>
       </ul>
     </div>
