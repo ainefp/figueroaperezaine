3ab648960b57c15f7539cf240e8aa363599903cf
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-04 09:30:38 +0100

    eliminado algo que no hace falta

--- src/server/server.js
+++ src/server/server.js
@@ -12,12 +12,9 @@ const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/bbdd';
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 app.listen(PORT, () => {
     console.log(`Server Express is running on port ${PORT}`);
