2b571250971f6cc36e58915e98198443d3d223fc
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-07 10:01:57 +0100

    sigo con el botón de editar

--- src/api/noticias.js
+++ src/api/noticias.js
@@ -10,6 +10,10 @@ export const addNoticia = (nuevaNoticia) => {
     return axios.post(API_URL, nuevaNoticia).then(res => res.data);
 }
 
+export const editNoticia = (id, noticiaActualizada) => {
+    return axios.put(`${API_URL}/${id}`, noticiaActualizada).then(res => res.data);
+}
+
 export const deleteNoticia = (id) => {
     return axios.delete(`${API_URL}/${id}`).then(res => res.data);
 }
