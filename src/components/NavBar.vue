ceb9697fc1e2e6777bb4943c4caa6c116877d067
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-21 09:14:02 +0100

    Juan carlos me pasa su navBar (descentrado)

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,14 @@
 <template>
-  <nav class="navbar navbar-dark bg-primary sticky-top navbar-expand-lg min-height-75">
+  <nav class="navbar navbar-dark bg-primary sticky-top navbar-expand-lg">
     <div class="container-fluid">
-      <!-- Marca o logo -->
-      <router-link id="icono" to="/" class="navbar-brand position-absolute ms-4"><img class="logo" src="/logo.svg" alt="Logo" /></router-link>
-
+      <!-- Logo -->
+      <router-link id="icono" to="/" class="navbar-logo ms-5">
+        <img class="logo" src="/logo.svg" alt="Logo" />
+      </router-link>
+        
       <!-- Botón de hamburguesa en pantallas pequeñas -->
       <button
-        id="btnPq"
-        class="navbar-toggler position-absolute end-0 mx-3 me-5"
+        class="navbar-toggler me-5"
         type="button"
         data-bs-toggle="collapse"
         data-bs-target="#navbarNav"
@@ -18,52 +19,84 @@
         <span class="navbar-toggler-icon"></span>
       </button>
 
-      <!-- Links de navegación -->
-      <div class="collapse navbar-collapse justify-content-center align-items-center" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link to="/" class="nav-link"> Inicio </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/clientes" class="nav-link"> Clientes </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/noticias" class="nav-link"> Noticias </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/modelos" class="nav-link"> Modelos </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/taller" class="nav-link"> Taller </router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/#" class="nav-link"> Contacto </router-link>
-          </li>
+      <!-- Enlaces centrados -->
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item"><router-link to="/" class="nav-link">Inicio</router-link></li>
+          <li class="nav-item"><router-link to="/clientes" class="nav-link">Clientes</router-link></li>
+          <li class="nav-item"><router-link to="/modelos" class="nav-link">Modelos</router-link></li>
+          <li class="nav-item"><router-link to="/taller" class="nav-link"> Taller </router-link></li>
+          <li class="nav-item"><router-link to="/ventas" class="nav-link">Ventas</router-link></li>
+          <li class="nav-item"><router-link to="/noticias" class="nav-link">Noticias</router-link></li>
+          <li class="nav-item"><router-link to="/contacto" class="nav-link">Contacto</router-link></li>
         </ul>
+
+        <!-- Dropdown de acceso/registro -->
+        <div class="dropdown ms-auto">
+          <button
+            class="btn btn-primary dropdown-toggle"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            <i class="bi bi-person fs-2"></i>
+          </button>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <!-- Mostra “Acceso/Registro” se NON hai usuario logueado -->
+            <li v-if="!isLogueado"><router-link class="dropdown-item" to="/login">Acceso</router-link></li>
+            <li v-if="!isLogueado"><router-link class="dropdown-item" to="/clientes">Registro</router-link></li>
+            <!-- Mostra “Cerrar Sesión” se está logueado -->
+            <li v-if="isLogueado">
+              <a class="dropdown-item" href="#" @click.prevent="logout">Cerrar Sesión</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </nav>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+
+// Estado do login
+const isLogueado = ref(false)
+const userName = ref('')
+
+// Cando o componente se monta, le localStorage (para cando montes a autenticación)
+onMounted(() => {
+  isLogueado.value = localStorage.getItem('isLogueado') === 'true'
+  userName.value = localStorage.getItem('userName') || ''
+})
+
+// Logout
+function logout() {
+  // Borra datos de sesión do localStorage
+  localStorage.removeItem('isLogueado')
+  localStorage.removeItem('userName')
+  localStorage.removeItem('isAdmin')
+  localStorage.removeItem('isUsuario')
+
+  // Actualiza estado
+  isLogueado.value = false
+  userName.value = ''
+
+  // Redirixe ao inicio recargando a páxina
+  window.location.href = '/'
+}
 </script>
 
 <style>
-  .navbar {
-    min-height: 75px;
-  }
-
   .navbar-dark .nav-link {
-    color: rgba(255,255,255,0.9);
+    color: rgba(255, 255, 255, 0.9);
   }
-
   .navbar-dark .nav-link:hover,
   .navbar-dark .nav-link:focus {
     color: #fff;
   }
 
   .logo {
-    width: 45%;
+    width: 40%;
   }
 
   /* Ajustes para pantallas pequeñas */
