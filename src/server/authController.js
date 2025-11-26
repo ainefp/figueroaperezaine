b81e7adddde4e75e4fa4bd3e150ab42c3c6fd60b
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-26 11:04:15 +0100

    espacio en blanco en authController

--- src/server/authController.js
+++ src/server/authController.js
@@ -26,6 +26,7 @@ export const login = async (req, res) => {
             apellido: user.apellido,
             tipo: user.tipo || 'user'
         });
+        
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Error interno del servidor' });
