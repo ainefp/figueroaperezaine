6707a00b78e5b362b7fc3f60320138490f05b4cf
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-26 11:22:30 +0100

    modificado server

--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import articulosRoutes from "./articulosRoutes.js"; // ruta al router backend
+import authRoutes from './authRoutes.js';
 
 dotenv.config();
 
@@ -16,13 +17,15 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST", "PUT", "DELETE"]
-// }));
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use("/api/articulos", articulosRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 mongoose.connect(MONGO_URI)
     .then(() => console.log('MongoDB connected'))
