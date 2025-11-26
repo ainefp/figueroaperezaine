16a3d51646d8647055ad0bc233d64b331b495771
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-26 13:48:39 +0100

    cambiadas cosas para que fuycoes

--- src/server/server.js
+++ src/server/server.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import articulosRoutes from "./articulosRoutes.js"; // ruta al router backend
-import authRoutes from './authRoutes.js';
+import authRoutes from './authRouter.js';
 
 dotenv.config();
 
