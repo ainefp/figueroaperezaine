46274a37a68e974107aea377cac2b2982d38ed30
Ainé Figueroa <aine.xena.100@gmail.com>
2025-11-28 09:15:56 +0100

    más cambios

--- src/server/authController.js
+++ src/server/authController.js
@@ -32,3 +32,34 @@ export const login = async (req, res) => {
         res.status(500).json({ message: 'Error interno del servidor' });
     }
 }
+
+/////////////// MEJOR EN UN FICHERO APARTE authMiddleware.js
+
+// Middleware: verificar JWT
+// Se usa en rutas que requieren autenticación
+// Verifica el token enviado en el header Authorization
+
+export const verificarToken = (req, res, next) => {
+  const authHeader = req.headers.authorization; // Authorization: Bearer <token>
+  if (!authHeader) return res.status(401).json({ mensaje: "Token no recibido" });
+
+  const token = authHeader.split(" ")[1]; // separar "Bearer" del token
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded; // guardar info del usuario en req
+    next(); // continuar al controlador
+  } catch (err) {
+    return res.status(403).json({ mensaje: "Token inválido" });
+  }
+};
+
+
+/// TAMBIEN EN EL FICHERO APARTE authMiddleware.js
+// Middleware: solo admin
+export const soloAdmin = (req, res, next) => {
+  if (req.user?.tipo !== "admin") {
+    return res.status(403).json({ mensaje: "Acceso solo para administradores" });
+  }
+  next();
+};
--- src/server/authRouter.js
+++ src/server/authRouter.js
@@ -1,8 +1,12 @@
-import { login } from './authController.js';
+import { login, soloAdmin, verificarToken } from './authController.js';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/login', login);
+
+router.get('/modelos', verificarToken, soloAdmin, (req, res) => {
+    res.json({ message: `Hola ${req.user.dni}, estás autenticado` });
+});
 
 export default router;
