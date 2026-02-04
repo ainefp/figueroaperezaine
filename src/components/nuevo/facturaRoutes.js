6c5ff3935eb2fdc05438342c795b06f17005c457
Ainé Figueroa <aine.xena.100@gmail.com>
2026-02-04 13:40:37 +0100

    añadidos js

---
+++ src/components/nuevo/facturaRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import Factura from "./models/Factura.js";
+
+const router = express.Router();
+
+// CREAR FACTURA
+router.post("/", async (req, res) => {
+  try {
+    console.log("Factura recibida:", req.body);
+
+    const factura = new Factura(req.body);
+    await factura.save();
+
+    res.status(201).json(factura);
+  } catch (error) {
+    console.error("Error al guardar factura:", error);
+    res.status(500).json({ error: "Error al guardar factura" });
+  }
+});
+
+//Obtener Facturas
+router.get("/", async (req, res) => {
+  const facturas = await Factura.find();
+  res.status(200).json(facturas);
+});
+export default router;
