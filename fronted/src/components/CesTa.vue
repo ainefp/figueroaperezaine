fd43f1caa8718293fd61efbb0b05433967ae6b94
Ainé Figueroa <aine.xena.100@gmail.com>
2026-02-11 18:59:14 +0100

    cesta

--- fronted/src/components/CesTa.vue
+++ fronted/src/components/CesTa.vue
@@ -1,5 +1,7 @@
 <template>
-    <h4 class="text-center my-1 bg-primary-subtle py-1"> Mi Cesta </h4>
+    <h4 class="text-center my-1 bg-primary-subtle py-1">
+        <i class="bi bi-cart3 me-2"></i> Mi Cesta
+    </h4>
     <div class="container-fluid my-1 p-3 pb-5 border rounded-3 shadow-sm bg-light ">
         <div v-if="cesta.items.length === 0" class="alert alert info">
             La cesta está vacía.
@@ -9,9 +11,9 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th>Produccto</th>
+                        <th>Producto</th>
                         <th>Precio</th>
-                        <th>Cantidad</th>
+                        <th colspan="3">Cantidad</th>
                         <th>Total</th>
                         <th></th>
                     </tr>
@@ -21,38 +23,101 @@
                         <td>{{ item.nombre }}</td>
                         <td>{{ item.precio }} €</td>
                         <td>
-                            <button class="btn btn-sm btn-outline-secondary me-1" @click="decrementar(item.id)">-</button>
+                            <button 
+                                class="btn btn-sm btn-outline-secondary me-1" 
+                                @click.stop="decrementar(item.id)"
+                            >-</button>
                             {{ item.cantidad }}
-                            <button class="btn btn-sm btn-outline-secondary me-1" @click="incrementar(item.id)">+</button>
+                            <button 
+                                class="btn btn-sm btn-outline-secondary me-1" 
+                                @click.stop="incrementar(item.id)"
+                            >+</button>
                         </td>
-                        <td>{{ item.precio * item.cantidad }} €</td>
+                        <td colspan="3">{{ item.precio * item.cantidad }} €</td>
                         <td>
-                            <button class="btn btn-sm btn-danger" @click="removeProducto(item.id)">Eliminar</button>
+                            <button 
+                            class="btn btn-sm btn-danger" 
+                            @click.stop="removeProducto(item.id)"
+                        >Eliminar</button>
                         </td>
                     </tr>
                 </tbody>
+                <tfoot>
+                    <tr class="fw-bold">
+                        <!-- <td>
+                            <label for="codigoDescuento">Introduce un codigo de descuento:</label>
+                            <input
+                                type="text"
+                                v-model="codigoDescuento"
+                                v-on:change="validarDescuento"
+                                :class="{ 'is-invalid': !cesta.codigoDescuento.valido }"
+                                ;
+                            />
+                        </td> -->
+                        
+                        <td>Precio de envio:</td>
+                        <td>
+                            {{
+                                cesta.envioGratis.envioGratis
+                                ? `${cesta.envioGratis.precioEnvio}€`
+                                : "GRATIS"
+                            }}
+                        </td>
+                        <td colspan="3" class="text-end">Total</td>
+                        <td>{{ cesta.precioFinal.toFixed(2) }}€</td>
+                        <td>
+                            <button
+                                class="btn btn-success btn-sm justify-content-end px-3"
+                                @click="iniciarPago"
+                                :disabled="!logueado"
+                            >
+                                Pago
+                            </button>
+                            </td>
+                        </tr>
+                    </tfoot>
             </table>
-
-            <div class="d-flex justify-content-between align-items-center mt-3">
-                <h5> Total: {{ cesta.totalPrecio }} €</h5>
-                <button class="btn btn-success" @click="iniciarPago">Iniciar Pago</button>
-            </div>
+            <span v-if="!logueado" class="mb-0 d-flex justify-content-end">
+                Por favor, para proceder al pago:
+                <router-link to="/login">Inicie Sesión</router-link> o
+                <router-link to="/clientes">Regístrate</router-link>
+            </span>
         </div>
     </div>
 </template>
 <script setup>
-    import { useCestaStore } from '@/store/cesta.js';
+    import { ref } from "vue";
     import axios from 'axios';
-    import { setMapStoreSuffix } from 'pinia';
+    import Swal from "sweetalert2";
+    import { useCestaStore } from '@/store/cesta.js';
+    
+    const logueado = sessionStorage.getItem("token") ? true : false;
     
     const cesta = useCestaStore();
+
+    const codigoDescuento = ref("");
 
     const incrementar = (id) => cesta.incrementar(id);
     const decrementar = (id) => cesta.decrementar(id);
     const removeProducto = (id) => cesta.removeProducto(id);
 
+    // function validarDescuento() {
+    //     cesta.codigoDescuento = codigoDescuento.value;
+    // }
+
+    // function mostrarAlerta(title, text, icon) {
+    //     Swal.fire({
+    //         icon: icon,
+    //         title: title,
+    //         text: text,
+    //         timer: 1500,
+    //         showConfirmButton: false,
+    //     });
+    // }
+
     const iniciarPago = async () => {
         if (!cesta.items.length) {
+            // mostrarAlerta("Aviso", "La cesta está vacía", "warning");
             return;
         }
 
@@ -65,13 +130,15 @@
             const session = response.data;
 
             if (!session.url) {
-                console.error('No se recibió URL de Stripe.')
+                console.error('No se recibió URL de Stripe.');
+                // mostrarAlerta("Error", "No se pudo iniciar el pago", "error");
                 return;
             }
 
             window.location.href = session.url;
         } catch (error) {
             console.error('Error en iniciarPago: ', error);
+            // mostrarAlerta("Error", "No se pudo iniciar el pago", "error");
         }
     }
 </script>
--- fronted/src/store/cesta.js
+++ fronted/src/store/cesta.js
@@ -1,49 +1,142 @@
-import { defineStore } from 'pinia';
+import { defineStore } from "pinia";
+import { ref, computed, watch } from "vue";
 
-export const useCestaStore = defineStore('cesta', {
-    state: () => ({
-        items: []
-    }),
-    
-    getters: {
-        totalItems(state) {
-            return state.items.reduce((total, item) => total + item.cantidad, 0)
-        },
-        totalPrecios(state) {
-            return state.items.reduce((total, item) => total + item.precio * item.cantidad, 0)
-        }
-    },
+//Creamos un objeto que utilice la dependencia Pinia y le asignamos los datos y los métodos que tendrá.
+export const useCestaStore = defineStore("cesta", () => {
+  // Creamos un objeto reactivo que almacenará los items que añadimos/quitamos de la cesta para ir cambiando su valor en ejecución
+  const items = ref([]);
+  //Segundo array vacío para que, al pagar, deje de verse visualmente la lista pero se mantenga para la factura.
+  const compraCompleta = ref([]);
 
-    actions: {
-        addProducto(producto) {
-            const existente = this.items.find(item => item.id === producto.id);
+  //Conseguir el número total de items añadidos a la lista
+  //total es el valor previo, empieza en 0 por el ultimo dato pasado a la funcion
+  const totalItems = computed(() => {
+    if (!Array.isArray(items.value)) return 0;
+    return items.value.reduce((total, item) => total + (item.cantidad || 0), 0);
+  });
 
-            if (existente) {
-                existente.cantidad++;
-            } else {
-                this.items.push({
-                    ...producto,
-                    cantidad: 1
-                })
-            }
-        },
+  //Calcula el precio total de todos los productos
+  const totalPrecio = computed(() => {
+    //Vemos si la lista principal esta vacia, si lo está, usamos la otra
+    const currentList =
+      items.value.length > 0 ? items.value : compraCompleta.value;
+    if (!Array.isArray(currentList)) return 0;
+    const total = currentList.reduce(
+      (total, item) => total + item.precio * (item.cantidad || 0),
+      0,
+    );
+    return total;
+  });
 
-        removeProducto(id) {
-            this.items = this.items.filter(item => item.id !== id);
-        },
-
-        incrementar(id) {
-            const item = this.items.find(item => item.id === id);
-            if (item) item.cantidad++;
-        },
-
-        decrementar(id) {
-            const item = this.items.find(item => item.id === id && item.cantidad > 1);
-            if (item) item.cantidad--;
-        },
-
-        limpiarCesta() {
-            this.items = [];
-        }
+  //Copia la lista a la lista para la factura y vacia la original(para visual)
+  function completarCompra() {
+    if (items.value.length > 0) {
+      compraCompleta.value = [...items.value];
+      items.value = [];
     }
-})
+  }
+  // Acción que añade un producto a la lista, y si ya existe, solo aumente su cantidad
+  function addProducto(producto) {
+    const existente = items.value.find((item) => item.id === producto.id);
+    if (existente) {
+      existente.cantidad = (existente.cantidad || 0) + 1;
+    } else {
+      items.value.push({
+        ...producto,
+        cantidad: 1,
+      });
+    }
+  }
+
+  //Funcion que elimina un producto de la lista
+  function removeProducto(id) {
+    const index = items.value.findIndex((item) => item.id === id);
+    if (index > -1) {
+      items.value.splice(index, 1); //splice MANTIENE reactividad
+    }
+  }
+
+  //Aumenta la cantidad de un producto en base a su id
+  function incrementar(id) {
+    const item = items.value.find((item) => item.id === id);
+    if (item) item.cantidad++;
+  }
+
+  //Funcion que reduce la cantidad de un item en concreto, si el item existe pero la cantidad es menor a 1, lo borra
+  function decrementar(id) {
+    const item = items.value.find((item) => item.id === id);
+    if (item && item.cantidad > 1) {
+      item.cantidad--;
+    } else if (item) {
+      removeProducto(id);
+    }
+  }
+
+  //Vacías la cesta y borra el sessionStorage(vacío real)
+  function clearCesta() {
+    items.value = [];
+    compraCompleta.value = [];
+    sessionStorage.removeItem("items");
+  }
+
+  //Cuando se crea el objeto mira si hay un items en el sessionStorage, si lo hay le asigna el valor a items.
+  const itemsData = sessionStorage.getItem("items");
+  if (itemsData) {
+    try {
+      items.value = JSON.parse(itemsData);
+    } catch (e) {
+      console.error("Error parsing cart:", e);
+    }
+  }
+
+  //Precio que se vera post descuentos/envio
+  const precioFinal = ref(0);
+  const codigoDescuento = ref({ valido: false, codigo: "" });
+  const envioGratis = ref({ precioEnvio: 50, envioGratis: true });
+
+  //Cada vez que cambie el precioTotal o el codigoDescuento se actualiza el precioFinal (actualmente solo se comprueba en lo visual)
+  watch(
+    [codigoDescuento, totalPrecio],
+    () => {
+      let precio = totalPrecio.value;
+      envioGratis.value.envioGratis = precio >= 50;
+      if (!envioGratis.value.envioGratis) {
+        precio += envioGratis.value.precioEnvio;
+      }
+      if (codigoDescuento.value.codigo === "DESCUENTO") {
+        codigoDescuento.value.valido = true;
+        precio *= 0.9;
+      } else {
+        codigoDescuento.value.valido = false;
+      }
+
+      precioFinal.value = precio;
+    },
+    { immediate: true, deep: true },
+  );
+
+  // Cada vez que un item nuevo entre en items, se cambiara el sessionSotrage para que entre este mismo, newItems es una referencia al array real
+  watch(
+    items,
+    (newItems) => {
+      sessionStorage.setItem("items", JSON.stringify(newItems));
+    },
+    { deep: true },
+  );
+
+  return {
+    items,
+    compraCompleta,
+    totalItems,
+    totalPrecio,
+    precioFinal,
+    envioGratis,
+    codigoDescuento,
+    addProducto,
+    removeProducto,
+    incrementar,
+    decrementar,
+    clearCesta,
+    completarCompra,
+  };
+});
